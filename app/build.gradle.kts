9012a2abb3c93c695726b5de5cbae5b6157adabe
Parth N Mistry <parth9773021717@gmail.com>
2024-01-04 12:57:40 +0530

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.github.bump tech.glide:glide:4.12.0")
     implementation("com.google.code.gson:gson:2.9.1")
     implementation("com.android.volley:volley:1.2.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
